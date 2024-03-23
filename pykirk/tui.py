24bae2291a89570c991d860ddd072037a3195467
Jim Kutter <rwg1138@gmail.com>
2024-03-23 00:00:50 -0400

    use directorytree instead of tree

--- pykirk/tui.py
+++ pykirk/tui.py
@@ -1,29 +1,22 @@
 import configparser
+import os
 import pathlib
 
 from textual.app import App, ComposeResult, RenderResult
 from textual.containers import Horizontal, Vertical, VerticalScroll
 from textual.widget import Widget
-from textual.widgets import Footer, Header, Placeholder, Static, Tree
+from textual.widgets import Footer, Header, Placeholder, Static, DirectoryTree
 
 
 class DateWidget(Widget):
     def __init__(self, base_path: str):
         super().__init__()
-        self.base_path = pathlib.Path(base_path)
+        self.base_path = pathlib.Path(os.path.expanduser(base_path))
 
         self.base_path.mkdir(exist_ok=True)
 
     def compose(self) -> ComposeResult:
-        tree: Tree[dict] = Tree("/")
-        tree.root.expand()
-        for entry in self.base_path.iterdir():
-            tree.root.add_leaf(entry.name)
-        # characters = tree.root.add("Characters", expand=True)
-        # characters.add_leaf("Paul")
-        # characters.add_leaf("Jessica")
-        # characters.add_leaf("Chani")
-        yield tree
+        yield DirectoryTree(self.base_path)
 
 
 class PreviewWidget(Static):
@@ -50,7 +43,7 @@ class PyKirk(App):
 
     def init_config(self):
         self.config = configparser.ConfigParser()
-        config_file_path = pathlib.Path("~/.pykirk/config.ini").resolve()
+        config_file_path = pathlib.Path(os.path.expanduser("~/.pykirk/config.ini"))
 
         config_file_path.parent.mkdir(exist_ok=True)
 
